69d88d22af115bbb6a02f4359c1d1e4dd138b100
Luigi11162 <luigibacco01@gmail.com>
2022-12-28 12:56:34 +0100

    Setup Database
    Added relationship to tables, creation of database works, need help for creating tables

--- Website/flaskr/__init__.py
+++ Website/flaskr/__init__.py
@@ -1,24 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
-db = SQLAlchemy()
 DB_NAME= "beehave.db"
-
+db = SQLAlchemy()
 """Inizializzazione"""
 def create_app():
     app = Flask(__name__)
+
     """chiave segreta per criptare cookies"""
     app.config['SECRET_KEY'] = 'BEEHAVE'
-    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config['CKEDITOR_FILE_UPLOADER'] = 'upload'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/beehavedb'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    create_database(app)
     db.init_app(app)
 
-    from .model import Cliente
-    create_database(app)
+    from . import model
+
+    with app.app_context():
+        db.create_all()
 
     return app
 
 def create_database(app):
-    db.create_all(app=app)
-    print("Create database")
+    mysql_engine = create_engine('mysql://root:root@localhost')
+    existing_databases = mysql_engine.execute("SHOW DATABASES;")
+    existing_databases = [d[0] for d in existing_databases]
+
+    if "beehavedb" not in existing_databases:
+        mysql_engine.execute("CREATE DATABASE beehavedb")
+        print("DATABASE CREATO")
+
--- Website/flaskr/model/TicketAdozione.py
+++ Website/flaskr/model/TicketAdozione.py
@@ -1,8 +1,8 @@
 from Website.flaskr import db
-
+from sqlalchemy.sql import func
 
 class TicketAdozione(db.Model):
    cliente = db.Column(db.String(45), db.ForeignKey('cliente.email'), primary_key=True)
    id_alveare = db.Column(db.Integer, db.ForeignKey('alveare.id'), primary_key=True)
    percentuale_produzione = db.Column(db.Integer, nullable=False)
-   data_inizio_adozione = db.Column(db.Datetime(timezone=True), nullable=False)
+   data_inizio_adozione = db.Column(db.Datetime(timezone=True), default=func.now(), nullable=False)
