f08bf44a8355fb84685e94f2e1874b607e8bd660
Luigi11162 <luigibacco01@gmail.com>
2022-12-29 20:26:14 +0100

    Added Service module

--- Website/flaskr/gestione_utente/GestioneUtenteController.py
+++ Website/flaskr/gestione_utente/GestioneUtenteController.py
@@ -1,21 +1,20 @@
 from flask import Blueprint, request, redirect, url_for
-from Website.flaskr.model.Apicoltore import Apicoltore
-from Website.flaskr.model.Cliente import Cliente
 from werkzeug.security import check_password_hash
 from flask_login import login_user, logout_user
+from GestioneUtenteService import getApicoltoreByEmail, getClienteByEmail
 
 app= Blueprint('app', __name__)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method=='POST':
-        apicoltore=request.form.get('utente')
-        email=request.form.get('email')
-        pwd=request.form.get('password')
+        apicoltore = request.form.get('utente')
+        email = request.form.get('email')
+        pwd = request.form.get('password')
         if apicoltore:
-            user=Apicoltore.query.filter_by(email=email).first()
+            user = getApicoltoreByEmail(email)
         else:
-            user = Cliente.query.filter_by(email=email).first()
+            user = getClienteByEmail(email)
         if user and check_password_hash(user.password, pwd):
             login_user(user, remember=True)
     return redirect(url_for('base.html'))
---
+++ Website/flaskr/gestione_utente/GestioneUtenteService.py
@@ -0,0 +1,9 @@
+from Website.flaskr.model.Apicoltore import Apicoltore
+from Website.flaskr.model.Cliente import Cliente
+
+def getApicoltoreByEmail(email):
+    return Apicoltore.query.filter_by(email=email).first()
+
+def getClienteByEmail(email):
+    return Cliente.query.filter_by(email=email).first()
+
