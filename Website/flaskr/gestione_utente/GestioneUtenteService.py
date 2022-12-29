9019ced1ebb655a501dd53e6447066c54790c9c1
Luigi11162 <luigibacco01@gmail.com>
2022-12-29 22:27:15 +0100

    Added Routes, Modified templates

--- Website/flaskr/gestione_utente/GestioneUtenteService.py
+++ Website/flaskr/gestione_utente/GestioneUtenteService.py
@@ -1,9 +1,10 @@
 from Website.flaskr.model.Apicoltore import Apicoltore
 from Website.flaskr.model.Cliente import Cliente
 
+
 def getApicoltoreByEmail(email):
     return Apicoltore.query.filter_by(email=email).first()
 
+
 def getClienteByEmail(email):
     return Cliente.query.filter_by(email=email).first()
-
