7e9e0a28de186943691435c8b7801051778ad549
Luigi11162 <luigibacco01@gmail.com>
2022-12-31 12:49:02 +0100

    Updated login, Added static folder, Added UtenteRegistrato ABC

--- Website/flaskr/model/Cliente.py
+++ Website/flaskr/model/Cliente.py
@@ -1,20 +1,12 @@
-from flask_login import UserMixin
+from .UtenteRegistrato import UtenteRegistrato
 from .. import db
 from .Acquisto import Acquisto
 from .TicketAdozione import TicketAdozione
 from .TicketAssistenza import TicketAssistenza
 
 
-class Cliente(db.Model, UserMixin):
+class Cliente(UtenteRegistrato):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(45), unique=True, nullable=False)
-    password = db.Column(db.String(300), nullable=False)
-    nome = db.Column(db.String(45), nullable=False)
-    cognome = db.Column(db.String(45), nullable=False)
-    indirizzo = db.Column(db.String(50), nullable=False)
-    citta = db.Column(db.String(45), nullable=False)
-    cap = db.Column(db.Integer, nullable=False)
-    telefono = db.Column(db.String(10), nullable=False)
     ticket_assistenza = db.relationship('TicketAssistenza', backref='cliente', lazy=True)
     ticket_adozione = db.relationship('TicketAdozione', backref='cliente', lazy=True)
     acquisto = db.relationship('Acquisto', backref='cliente', lazy=True)
---
+++ Website/flaskr/model/UtenteRegistrato.py
@@ -0,0 +1,15 @@
+from flask_login import UserMixin
+
+from .. import db
+
+
+class UtenteRegistrato(db.Model, UserMixin):
+    __abstract__ = True
+    email = db.Column(db.String(45), unique=True, nullable=False)
+    password = db.Column(db.String(300), nullable=False)
+    nome = db.Column(db.String(45), nullable=False)
+    cognome = db.Column(db.String(45), nullable=False)
+    indirizzo = db.Column(db.String(50), nullable=False)
+    citta = db.Column(db.String(45), nullable=False)
+    cap = db.Column(db.Integer, nullable=False)
+    telefono = db.Column(db.String(10), nullable=False)
