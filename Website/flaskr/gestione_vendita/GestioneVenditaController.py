39dd56d6972ff148b51f405de65a7527ff288852
andesrule <106107786+andesrule@users.noreply.github.com>
2023-01-04 23:26:20 +0100

    function acquista_prodotto implementation

--- Website/flaskr/gestione_vendita/GestioneVenditaController.py
+++ Website/flaskr/gestione_vendita/GestioneVenditaController.py
@@ -4,10 +4,11 @@ import os
 
 from werkzeug.utils import secure_filename
 from Website.flaskr import image_folder_absolute
-from Website.flaskr.Routes import area_personale
+from Website.flaskr.Routes import area_personale, home
 from Website.flaskr.gestione_utente.GestioneUtenteService import getApicoltoreById
 from Website.flaskr.gestione_vendita.GestioneVenditaService import inserisci_prodotto, getProdottoById, updateImage, \
-    get_ProdottiByApicoltore, deleteProdotto
+    get_ProdottiByApicoltore, deleteProdotto, acquisto_prodotto
+from Website.flaskr.model.Acquisto import Acquisto
 from Website.flaskr.model.Prodotto import Prodotto
 
 gv = Blueprint('gv', __name__)
@@ -79,7 +80,28 @@ def elimina_prodotto(id_prodotto, id_api):
         path = os.path.join(image_folder_absolute, prod.img_path)
         os.remove(path)
         deleteProdotto(id_prodotto)
-        prodotti_in_vendita = get_ProdottiByApicoltore(id_api)
-        #return render_template('/catalogo_apicoltore.html', prodotti_in_vendita=prodotti_in_vendita)
+        # prodotti_in_vendita = get_ProdottiByApicoltore(id_api)
+        # return render_template('/catalogo_apicoltore.html', prodotti_in_vendita=prodotti_in_vendita)
         return area_personale()
-        #TODO fix refresh page
+        # TODO fix refresh page
+
+
+@gv.route('/acquista_prodotto', methods=['POST', 'GET'])
+@login_required
+def acquista_prodotto():
+    if request.method == 'POST':  # TODO Add session isCliente
+        quantita = int(request.form.get('quantita_prod'))
+        print(quantita)
+        qnt_articolo = int(request.form.get('qnt_articolo'))
+        print(qnt_articolo)
+        id_cliente = int(request.form.get('id_client'))
+        print(id_cliente)
+
+        id_prodotto = int(request.form.get('id_prd'))
+        print(id_prodotto)
+        if quantita <= qnt_articolo:
+            flash('Quantitá non disponibile!', category='error')
+
+        acquisto = Acquisto(id_cliente=id_cliente, id_prodotto=id_prodotto)
+        acquisto_prodotto(acquisto, quantita)
+    return home()
