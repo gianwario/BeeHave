e648d26f05ff8957595817deea59a0a434ad9eaa
Irene Gaita <i.gaita1@studenti.unisa.it>
2023-01-10 17:49:20 +0100

    fix nav bar

--- Website/flaskr/static/js/homepage.js
+++ Website/flaskr/static/js/homepage.js
@@ -1,6 +1,14 @@
-const burger = document.getElementById('burger');
 const navLinks = document.querySelector('.nav-links');
+const burger = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
 
+// Event listeners
 burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+    navLinks.classList.toggle('nav-active');
+    burger.classList.toggle('bar-change');
+})
+
+// Sticky navbar
+window.addEventListener('scroll', () => {
+    navbar.classList.toggle('sticky', scrollY > 0)
 })
