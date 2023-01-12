e27de8b66d8fac4334524b5c8d4cdd627a33e3c0
Luigi11162 <luigibacco01@gmail.com>
2023-01-12 12:40:18 +0100

    Correct futher build hints
    Correct imports

--- Website/flaskr/model/TicketAssistenza.py
+++ Website/flaskr/model/TicketAssistenza.py
@@ -8,6 +8,6 @@ class TicketAssistenza(db.Model):
     id_apicoltore = db.Column(db.Integer, db.ForeignKey('apicoltore.id'), primary_key=True)
     nome = db.Column(db.String(45), nullable=False)
     descrizione = db.Column(db.String(200), nullable=False)
-    data_inizio = db.Column(db.DateTime(timezone=True),default=func.now(), nullable=False)
+    data_inizio = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
     data_archiviazione = db.Column(db.DateTime(timezone=True))
     stato = db.Column(db.String(45), nullable=False)
