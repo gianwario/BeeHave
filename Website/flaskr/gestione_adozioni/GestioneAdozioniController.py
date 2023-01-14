aa084942557d0922d9761e10604bccd8fda7a796
Luigi11162 <luigibacco01@gmail.com>
2023-01-14 15:51:32 +0100

    Added more controls to hive section

--- Website/flaskr/gestione_adozioni/GestioneAdozioniController.py
+++ Website/flaskr/gestione_adozioni/GestioneAdozioniController.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import Blueprint, request, session, flash
 from flask_login import login_required, current_user
 
-from Website.flaskr.Routes import mostra_alveari
+from Website.flaskr.Routes import mostra_alveari, modifica_stato, home
 from Website.flaskr.gestione_adozioni.GestioneAdozioniService import inserisci_alveare, affitto_alveare, aggiorna_stato
 from Website.flaskr.model.Alveare import Alveare
 from Website.flaskr.model.TicketAdozione import TicketAdozione
@@ -16,31 +16,31 @@ ga = Blueprint('ga', __name__)
 def inserimento_alveare():
     if request.method == 'POST' and session['isApicoltore']:
         nome = request.form.get('nome')
-        produzione = int(request.form.get('produzione'))
-        numero_api = int(request.form.get('numero_api'))
+        produzione = request.form.get('produzione')
+        numero_api = request.form.get('numero_api')
         tipo_miele = request.form.get('tipo_miele')
         tipo_fiore = request.form.get('tipo_fiore')
-        prezzo = float(request.form.get('prezzo'))
+        prezzo = request.form.get('prezzo')
         apicoltore = current_user.id
 
-        if not 0 < len(nome) <= 30:
+        if nome is None or not 0 < len(nome) <= 30:
             flash('Lunghezza Nome non valida!', category='error')
-        elif not 0 < len(tipo_fiore) <= 30:
+        elif tipo_fiore is None or not 0 < len(tipo_fiore) <= 30:
             flash('Lunghezza di TipoFiore non valida!', category='error')
-        elif not 0 < produzione <= 2000:
+        elif produzione is None or not produzione.isdigit() or not 0 < int(produzione) <= 2000:
             flash('Quantità produzione non è nel range corretto!', category='error')
-        elif not 0 < len(tipo_miele) <= 30:
+        elif tipo_miele is None or not 0 < len(tipo_miele) <= 30:
             flash('Lunghezza di TipoMiele non valida!', category='error')
-        elif not 0 < numero_api <= 40000:
+        elif numero_api is None or not numero_api.isdigit() or not 0 < int(numero_api) <= 40000:
             flash('NumeroApi non è nel range corretto!', category='error')
-        elif not 0 < prezzo <= 1000:
+        elif prezzo is None or not prezzo.isdigit() or not 0 < float(prezzo) <= 1000:
             flash('Prezzo non è nel range corretto!', category='error')
         else:
             flash('Inserimento avvenuto con successo!', category='success')
 
-            alveare = Alveare(nome=nome, produzione=produzione, numero_api=numero_api, tipo_miele=tipo_miele,
+            alveare = Alveare(nome=nome, produzione=int(produzione), numero_api=int(numero_api), tipo_miele=tipo_miele,
                               percentuale_disponibile=100,
-                              prezzo=prezzo, tipo_fiore=tipo_fiore, id_apicoltore=apicoltore)
+                              prezzo=float(prezzo), tipo_fiore=tipo_fiore, id_apicoltore=apicoltore)
 
             inserisci_alveare(alveare)
 
@@ -58,20 +58,22 @@ def modifica_stato_alveare():
         stato_larve = request.form.get('stato_larve')
         alveare_id = request.form.get('alveare_id')
 
-        if covata_compatta == '':
+        if covata_compatta is None:
             flash('CovataCompatta non è stata inserita!', category='error')
-        elif not 0 < len(popolazione) <= 30:
+        elif popolazione is None or not 0 < len(popolazione) <= 30:
             flash('Lunghezza di Popolazione non valida!', category='error')
-        elif not 0 < len(polline) <= 2000:
+        elif polline is None or not 0 < len(polline) <= 2000:
             flash('Lunghezza di Polline non valida!', category='error')
-        elif not 0 < len(stato_cellette) <= 30:
+        elif stato_cellette is None or not 0 < len(stato_cellette) <= 30:
             flash('Lunghezza di Stato Cellette non valida!', category='error')
-        elif not 0 < len(stato_larve) <= 30:
+        elif stato_larve is None or not 0 < len(stato_larve) <= 30:
             flash('Lunghezza di Stato Larve non valida!', category='error')
         else:
             aggiorna_stato(alveare_id, int(covata_compatta), popolazione, polline, stato_cellette, stato_larve)
             flash('Stato alveare aggiornato correttamente', category='success')
-    return mostra_alveari()
+            return mostra_alveari()
+        return modifica_stato(alveare_id)
+    return home()
 
 
 @ga.route('/adotta_alveare', methods=['POST', 'GET'])
