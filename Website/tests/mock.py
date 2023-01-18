51284908d6c39f3a58d8083a4b9b77495ed18c99
Irene Gaita <i.gaita1@studenti.unisa.it>
2023-01-18 15:19:24 +0100

    fix testing

--- Website/tests/mock.py
+++ Website/tests/mock.py
@@ -2,14 +2,14 @@ import pytest
 from flask import session
 from flask_login import login_user
 
-from Website.flaskr import create_app
+from Website.flaskr import create_app, db
 from Website.flaskr.Routes import views
 from Website.flaskr.model.Apicoltore import Apicoltore
 
 
 @views.route('/mock_login_apicoltore')
 def mock_login_apicoltore():
-    user = Apicoltore(id=1, nome="nome", cognome="cognome", indirizzo="indirizzo", citta="citta", cap=84345,
+    user = Apicoltore(nome="nome", cognome="cognome", indirizzo="indirizzo", citta="citta", cap=84345,
                       telefono=4324324362,
                       email="email", assistenza=1, descrizione="descrizione", password="password")
     login_user(user, remember=True)
