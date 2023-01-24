f964b09ae430c12289e132e003e109c4103d7830
GameWame <ninofrassica55@gmail.com>
2023-01-24 17:14:53 +0100

    E-mail system

--- Website/flaskr/gestione_vendita/GestioneVenditaController.py
+++ Website/flaskr/gestione_vendita/GestioneVenditaController.py
@@ -1,11 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+
 from flask import Blueprint, request, session
 from flask_login import current_user, login_required
 
 from Website.flaskr.Routes import mostra_prodotti, mostra_articoli_in_vendita, info_articolo, inserimento_prodotto_page
+from Website.flaskr.gestione_utente.GestioneUtenteService import get_apicoltore_by_id, get_cliente_by_id
 from Website.flaskr.gestione_vendita.GestioneVenditaService import inserisci_prodotto, cancella_prodotto, \
     acquisto_prodotto
 
 gv = Blueprint('gv', __name__)
+
+email_sender = 'beehaveofficial@gmail.com'
+email_password = "aosryhipaiuafdev"
+em = EmailMessage()
 
 
 @gv.route('/inserimento_prodotto', methods=['GET', 'POST'])
@@ -32,13 +40,56 @@ def elimina_prodotto(id_prodotto, id_apicoltore):
         return mostra_articoli_in_vendita(current_user.id)
 
 
-@gv.route('/acquista_prodotto', methods=['POST', 'GET'])
+@gv.route('/acquista_prodotto/<int:id_apicoltore>/<int:id_cliente>', methods=['POST', 'GET'])
 @login_required
-def acquista_prodotto():
+def acquista_prodotto(id_apicoltore, id_cliente):
     if request.method == 'POST' and not session['isApicoltore']:
         quantita = request.form.get('quantita_prod')
         id_prodotto = request.form.get('id_prd')
 
         if acquisto_prodotto(id_prodotto=id_prodotto, quantita=quantita, cliente=current_user):
+            if not session['isApicoltore']:
+                apicoltore = get_apicoltore_by_id(id_apicoltore)
+                subject = 'Acquisto Effettuato!'
+                body = "Qualcuno ha comprato la tua merda!"
+                em['From'] = email_sender
+                em['To'] = apicoltore.email
+                em['Subject'] = subject
+                em.set_content(body)
+
+                smtp = smtplib.SMTP('smtp.gmail.com', 587)
+                smtp.connect("smtp.gmail.com", 587)
+
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.ehlo()
+                smtp.login(email_sender, email_password)
+                smtp.sendmail(email_sender, apicoltore.email, em.as_string())
+
+                smtp.quit()
+                em.clear()
+                """
+                    Email Cliente              
+                """
+                cliente = get_cliente_by_id(id_cliente)
+                body_client = "Hai speso soldi!"
+                em['From'] = email_sender
+                em['To'] = cliente.email
+                em['Subject'] = subject
+                em.set_content(body_client)
+
+                smtp = smtplib.SMTP('smtp.gmail.com', 587)
+                smtp.connect("smtp.gmail.com", 587)
+
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.ehlo()
+                smtp.login(email_sender, email_password)
+                smtp.sendmail(email_sender, cliente.email, em.as_string())
+
+                smtp.quit()
+                em.clear()
+
             return info_articolo(id_prodotto)
+
     return mostra_prodotti()
