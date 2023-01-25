bd0f7457ffc7e4752bb6fb2688ad05363eca3696
Luigi11162 <luigibacco01@gmail.com>
2023-01-25 13:20:01 +0100

    Fixed image bug after purchase

--- Website/flaskr/gestione_vendita/GestioneVenditaController.py
+++ Website/flaskr/gestione_vendita/GestioneVenditaController.py
@@ -1,9 +1,13 @@
+import smtplib
+from email.message import EmailMessage
+
 from flask import Blueprint, request, session
 from flask_login import current_user, login_required
 
 from Website.flaskr.Routes import mostra_prodotti, mostra_articoli_in_vendita, info_articolo, inserimento_prodotto_page
+from Website.flaskr.gestione_utente.GestioneUtenteService import get_apicoltore_by_id, get_cliente_by_id
 from Website.flaskr.gestione_vendita.GestioneVenditaService import inserisci_prodotto, cancella_prodotto, \
-    acquisto_prodotto
+    acquisto_prodotto, get_prodotto_by_id
 
 gv = Blueprint('gv', __name__)
 
@@ -41,7 +45,54 @@ def acquista_prodotto():
     if request.method == 'POST' and not session['isApicoltore']:
         quantita = request.form.get('quantita_prod')
         id_prodotto = request.form.get('id_prd')
+        prodotto = get_prodotto_by_id(int(id_prodotto))
+        id_apicoltore = prodotto.id_apicoltore
+        apicoltore = get_apicoltore_by_id(id_apicoltore)
+        cliente = get_cliente_by_id(current_user.id)
+        prezzo = str(prodotto.prezzo)
+        totale = float(quantita) * float(prezzo)
 
         if acquisto_prodotto(id_prodotto=id_prodotto, quantita=quantita, cliente=current_user):
-            return info_articolo(id_prodotto)
+            if not session['isApicoltore']:
+                subject = 'Prodotto venduto!'
+                body = ('Ciao ' + apicoltore.nome + ',\nIl tuo articolo "' + prodotto.nome + '" è stato venduto.\n'
+                                                                                             'I dati dell\'acquirente sono:\n' + cliente.nome + ", " + cliente.cognome + "\n" + cliente.email
+                        + "\n" + cliente.telefono + "\n\nTi invitiamo a contattare il Cliente per accordarvi "
+                                                    "sulle modalità di pagamento.")
+                invia_email(subject, body, apicoltore.email)
+                """
+                    Email Cliente              
+                """
+                subject_client = 'Prodotto acquistato!'
+                body_client = ('Grazie per l\'acquisto!\nEcco un resoconto del tuo acquisto di ' + prodotto.nome
+                               + ":\nPrezzo: " + prezzo + " €\nQuantità: " + quantita + "\nTotale: " + str(totale)
+                               + " €\n\n Acquistato da: " + apicoltore.nome + "(" + apicoltore.email + ")" +
+                               "\n Grazie per aver supportato questo apicoltore e le sue api!")
+
+                invia_email(subject_client, body_client, cliente.email)
+                return info_articolo(id_prodotto)
+
     return mostra_prodotti()
+
+
+def invia_email(oggetto, corpo, email):
+    email_sender = 'beehaveofficial@gmail.com'
+    email_password = "aosryhipaiuafdev"
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email
+    em['Subject'] = oggetto
+
+    em.set_content(corpo)
+
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.connect("smtp.gmail.com", 587)
+
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email, em.as_string())
+
+    smtp.quit()
+    em.clear()
